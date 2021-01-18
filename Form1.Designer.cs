82d1e2b1b443fb28dd51aee85ab0ea2d642cbcea
Karolina Roszkowska <inmatrixos@gmail.com>
2021-01-18 21:46:08 +0000

    first form and db sqllite connection to display table records in data grid view

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Constructor_WinForms_SqlLite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProjects = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProjects)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProjects
+            // 
+            this.dgvProjects.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProjects.Location = new System.Drawing.Point(12, 35);
+            this.dgvProjects.Name = "dgvProjects";
+            this.dgvProjects.Size = new System.Drawing.Size(776, 312);
+            this.dgvProjects.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(64, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Display Projects";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvProjects);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProjects)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProjects;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace Constructor_WinForms_SqlLite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection dbConnection = new SQLiteConnection("Data Source=|DataDirectory|/db/db.db; version=3");
+
+            //if (!File.Exists("./db.db"))
+            //{
+            //    SQLiteConnection.CreateFile("db.db");
+            //}
+
+            string dbQuery = "SELECT * FROM Projects";
+            SQLiteCommand dbCommand = new SQLiteCommand(dbQuery, dbConnection);
+            DataTable dataTable = new DataTable();
+            SQLiteDataAdapter dbAdapter = new SQLiteDataAdapter(dbCommand);
+            dbAdapter.Fill(dataTable);
+            dgvProjects.DataSource = dataTable;
+        }
+    }
+}
